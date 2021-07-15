5433d646ac7404d5c0767c195b40a0475fb4473f
DustinTran305 <72329875+DustinTran305@users.noreply.github.com>
2021-07-15 10:05:38 +0700

    Update maclife.sh

--- maclife.sh
+++ maclife.sh
@@ -1,7 +1,7 @@
 csrutil authenticated-root disable;
-diskutil mount /Volumes/Macintosh\ HD;
- Mount -uv /Volumes/Macintosh\ HD;	
- cd /Volumes/Macintosh\ HD/System/Library/LaunchAgents;
+diskutil mount /Volumes/Physical\ Store\ disk0s2;
+ Mount -uv /Volumes/Physical\ Store\ disk0s2;	
+ cd /Volumes/Physical\ Store\ disk0s2/System/Library/LaunchAgents;
 mkdir tmp;
 mv com.apple.ManagedClientAgent.* tmp/;
 mv com.apple.mdmclient.* tmp/;
@@ -9,5 +9,5 @@ cd ../LaunchDaemons;
 mkdir tmp;
 mv com.apple.ManagedClient.* tmp/;
 mv com.apple.mdmclient.* tmp/;
-bless --folder /Volumes/Macintosh\ HD/System/Library/CoreServices --bootefi --create-snapshot;
+bless --folder /Volumes/Physical\ Store\ disk0s2/System/Library/CoreServices --bootefi --create-snapshot;
 Echo "Copyright Maclife.vn nhe"
